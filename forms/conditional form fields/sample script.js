701c881f030aaa25d11d18ca75c5a9b9ccba7499
Hristo Hristov (Chris) <hthristov@mail.bg>
2017-06-15 15:56:26 +0300

    Create sample script.js

--- forms/conditional form fields/sample script.js
+++ forms/conditional form fields/sample script.js
@@ -1,7 +1,39 @@
 window.addEventListener("load", function () { 
-var fromNewForm = decodeURIComponent(window.location.href).indexOf('https://indeavr.sharepoint.com/sites/Smartsourcing/hhristov/hp/Lists/Aanvrager/NewForm.aspx') !== -1;   
-var fromEditForm = decodeURIComponent(window.location.href).indexOf('https://indeavr.sharepoint.com/sites/Smartsourcing/hhristov/hp/Lists/Aanvrager/EditForm.aspx') !== -1;
-    if (fromNewForm || fromEditForm) {        //hide all necessary fields at page load        var huidigRow = document.querySelector('span[id*=Huidig_x0020_registratienr]');        huidigRow.parentElement.parentElement.style.display = "none"
-        var registratienrRow = document.querySelector('span[id*=Registratienr_x0020_oud_x0020_pr]');        registratienrRow.parentElement.parentElement.style.display = "none"
-        var internRow = document.querySelector('span[id*=Intern_x0020_referentienummer_x0]');        internRow.parentElement.parentElement.style.display = "none"
-        var allRadioButtons = document.querySelectorAll('input[id*=Bijzondere_x0020_gebruikskenmerk]');        for (radio in allRadioButtons) {            allRadioButtons[radio].onclick = function () {                if (this.value === 'Reeds in gebruik op andere locatie' ||                    this.value === 'Reeds in gebruik maar onder andere omstandigheden') {                    huidigRow.parentElement.parentElement.style.display = "table-row";                } else {                    huidigRow.parentElement.parentElement.style.display = "none";                }    if (this.value === 'Vervangt ander product') {     registratienrRow.parentElement.parentElement.style.display = "table-row";    }    else {     registratienrRow.parentElement.parentElement.style.display = "none";    }    if (this.value === 'Nieuw product enkel bestemd voor Materialen Labo'){     internRow.parentElement.parentElement.style.display = "table-row";    }    else {     internRow.parentElement.parentElement.style.display = "none";    }            }        }    }})
+var fromNewForm = decodeURIComponent(window.location.href).indexOf('') !== -1;   //put link to new form
+var fromEditForm = decodeURIComponent(window.location.href).indexOf('') !== -1; //put link to edit form here
+  
+    if (fromNewForm || fromEditForm) {  //check forms exist
+      
+      //hide all necessary fields at page load   
+      var elementToHide = document.querySelector('span[id*=<ID GOES HERE>]');     //take the DOM id of the element you want to hide
+      elementOne.parentElement.parentElement.style.display = "none"
+      
+      var elementToHideTwo = document.querySelector('span[id*=<ID GOES HERE>]');      //take the DOM id of the element you want to hide
+      elementToHideTwo.parentElement.parentElement.style.display = "none"
+      
+      var elementToHideThree = document.querySelector('span[id*=<ID GOES HERE>]');        //take the DOM id of the element you want to hide
+      elementToHideThree.parentElement.parentElement.style.display = "none"
+      
+      var allRadioButtons = document.querySelectorAll('input[id*=Bijzondere_x0020_gebruikskenmerk]');  //get an array of option in radio buttons 
+      for (radio in allRadioButtons) { 
+        allRadioButtons[radio].onclick = function () {   
+          if (this.value === 'radio button one text' || this.value === 'radio button two text') {        //edit this.value
+            elementOne.parentElement.parentElement.style.display = "table-row"; //display the relative element       
+          } else {    
+            elementOne.parentElement.parentElement.style.display = "none";      
+                 }  
+          if (this.value === 'radio button three text') {   //edit this.value
+            elementToHideTwo.parentElement.parentElement.style.display = "table-row";    
+                 }   
+          else {  
+            elementToHideTwo.parentElement.parentElement.style.display = "none"; 
+          }  
+          if (this.value === 'radio button four text'){   //edit this.value
+            elementToHideThree.parentElement.parentElement.style.display = "table-row"; 
+          }
+          else {   
+            elementToHideThree.parentElement.parentElement.style.display = "none";  
+          }          
+        }     
+      }    
+    }})
